e452c07451eb4ce34d03008b204e85c161d0d52f
Fvdp <fredvandenplas@gmail.com>
2018-05-05 01:37:11 +0200

    Ajout page index.php

---
+++ index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <title>Projet Zulgate</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Custom built theme - This already includes Bootstrap 4 -->
+  <link rel="stylesheet" href="css/mytheme.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container-fluid pl-0">
+        <div class="row">
+            <div class="col-md-12 w-auto bg-light">
+                <nav class="navbar sticky-top">
+                    <a class="navbar-brand" href="#">Zulgate</a>
+                    <ul class="nav justify-content-end">
+                        <li>
+                            <a class="nav-link" href="#"><i class="fa fa-envelope-o"></i></a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Se connecter</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+<!--    fin row header-->
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 bg bg-light mh-100">
+                <div class="sidebar-sticky">
+                    <a class="nav flex-column">
+                        <a class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Recherche...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </a>
+                        <a class="nav-link dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Afficher les Liens</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </a>
+                        <a>
+                            <a class="nav-link" href="#">Tags</a>
+                        </a>
+                    </a>
+                </div>            
+            </nav>
+<!--        fin menu gauche-->
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+            <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+            </div>
+            <div class="row">
+                <nav class="col-md-2 bg bg-light mh-100">
+                <div class="sidebar-sticky">
+                    <a class="nav flex-column">
+                        <a class="sidebar-search">
+
+                        </a>
+                        <a class="nav-link dropdown">
+ 
+                        </a>
+                        <a>
+
+                        </a>
+                    </a>
+                </div>            
+            </nav>
+<!--        fin menu gauche-->
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Liens</h5>
+                            <p class="card-text">Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+<!--        fin section-->
+        <footer class="row ">
+            <nav class="navbar fixed-bottom navbar-light bg-light">
+                <a class="nav" href="#">Crédit</a>
+            </nav>
+        </footer>
+    </div>
+<!--    fin container-->
+</body>
+</html>
