d3f4553c83249a71e908a20626ec1fa22bd75cf0
Fvdp <fredvandenplas@gmail.com>
2018-05-24 08:49:58 +0200

    Rajout et modification pour intégration maquette pour symfony

--- src/Controller/LinksController.php
+++ src/Controller/LinksController.php
@@ -10,8 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Form\AddLinkType;
 
+/** controller pour afficher, rajouter et éditer les liens */
 class LinksController extends Controller
 {
+/** Méthode pour afficher tous les liens sur page accueil */
     /**
      * @Route("/", name="home")
      */
@@ -25,6 +27,7 @@ class LinksController extends Controller
         ]);
     }
 
+/** Méthode pour afficher le détail d'un lien */
     /**
      * @Route("/view/{id}", name="view")
      */
@@ -35,6 +38,9 @@ class LinksController extends Controller
         ]);
     }
 
+/** Méthode pour rajouter un lien dans mysql
+ *  si ok redirection vers page accueil
+ */
     /**
      * @Route("/add", name="add")
      */
@@ -50,7 +56,7 @@ class LinksController extends Controller
 	        $em->persist($link);
 	        $em->flush();
 
-	        return $this->redirectToRoute('add');
+	        return $this->redirectToRoute('home');
 	    }
 
         return $this->render('links/add.html.twig', [
@@ -58,6 +64,9 @@ class LinksController extends Controller
         ]);
     }
 
+/** Méthode pour éditer le lien
+ *  si ok redirection vers le détail du lien
+ */
     /**
 	 * @Route("/edit/{id}", name="edit")
 	 */ 
@@ -71,10 +80,38 @@ class LinksController extends Controller
 	        $em->persist($link);
 	        $em->flush();
 
-	        return $this->redirectToRoute('home');
+            return $this->render('links/view.html.twig', [
+                'link'  =>  $link
+            ]);
 	    }
 
 	    return $this->render('links/add.html.twig', [
+	        'form'  =>  $form->createView()
+	    ]);
+    }
+    
+    /** Méthode pour effacer le lien
+ *  si ok redirection vers page accueil
+ */
+    /**
+	 * @Route("/erase/{id}", name="erase")
+	 */ 
+	public function erase(Links $link, Request $request){
+	    $form = $this->createForm(AddLinkType::class, $link);
+
+	    $form->handleRequest($request);
+	    
+	    if($form->isSubmitted() && $form->isValid()){
+	        $em = $this->getDoctrine()->getManager();
+	        $em->remove($link);
+	        $em->flush();
+
+            return $this->render('links/view.html.twig', [
+                'link'  =>  $link
+            ]);
+	    }
+
+	    return $this->render('links/erase.html.twig', [
 	        'form'  =>  $form->createView()
 	    ]);
 	}
